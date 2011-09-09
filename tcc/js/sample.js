7c6c04c17c6dad441c3ae2fbcc2fd005acc8b7fc
apssouza <apssouza22@gmail.com>
2011-09-09 00:59:30 -0300

    modulo do aluno

---
+++ tcc/js/sample.js
@@ -0,0 +1,153 @@
+$('.window').fadeTo("slow",0);
+var core = core || {};
+core.window = {};
+
+core.window = function (win,title){
+
+    //settings.title settings.winDOW
+    classe = this;
+    var settings={};
+
+    settings.height = 500;
+    //POSITION OF ITEM TASK BAR
+    var leftItemBar;
+    var leftItemTop;
+    var windowLeft;
+    var windowTop;
+
+    this.showWindow= function()
+    {
+        settings.title   = title;
+        settings.win     = '#' + win;
+       // settings.name     = this.name;
+        settings.idItemTask = 'task' + win;
+        settings.width  = $(settings.win).width();
+      
+        if($('#'+settings.idItemTask).length == 0){
+            animateWindow(settings.win);
+            createItemTaskBar(settings.win)
+            listen();
+        }else{
+            animateWindow(settings.win);
+        }
+    }
+
+    //FN
+    var setPositionWindow = function(){
+        //armazena a largura e a altura da janela
+        var winH = $(window).height();
+        var winW = $(window).width();
+        
+        
+
+        //centraliza na tela a janela popup
+        windowTop = winH/2-settings.height/2;
+        windowTop = (windowTop > 0 ) ? windowTop : 5;
+        
+        windowLeft = winW/2-settings.width/2;
+    }
+   
+    //fn
+    var createItemTaskBar = function()
+    {
+        var itemTaskBar = $('.modeloItemBar').clone();
+        itemTaskBar.removeClass('modeloItemBar').attr('id',settings.idItemTask);
+        itemTaskBar.find('.centerItem').text(settings.title);
+        $('.taskBarListItems').append(itemTaskBar);
+        leftItemTop  = $('#taskbar').position().top;
+        leftItemBar = $('#'+settings.idItemTask).position().left;
+		itemTaskBar.bind("contextmenu",close);
+    }
+
+    //fn
+    var animateWindow = function(){
+        
+        setPositionWindow();
+       var params = {
+            top:windowTop,    left:windowLeft,    opacity:1,
+            height:settings.height,       width:settings.width
+        }
+        //DEFINE THE ZINDEX OF WINDOW
+        $('.window').css('zIndex', 10);
+        $(settings.win).css('zIndex', 20)
+
+        $(settings.win).animate(params, 500, 'swing', function(){        })
+    }
+
+    var minimizaWindow = function(){
+
+            $(settings.win).addClass('min');
+            var params = {
+                top:leftItemTop,left:leftItemBar,
+                opacity:0,height:0, width:0
+            }
+
+            $(settings.win).animate(params, 500, 'swing', function(){            })
+    }
+	//fn
+	var close = function(e){
+             e.preventDefault();
+            $(settings.win).css({top:leftItemTop,left:leftItemBar,height:0});
+            $(settings.win).fadeTo("slow",0);
+			$('.centerItem','#'+settings.idItemTask).unbind('click');
+            $('#'+settings.idItemTask).remove();
+        }
+
+    //fn
+    function listen()
+    {
+        //LISTEN BUTTON MINIMIZA
+        $('.min',settings.win).click(function(e){
+           e.preventDefault();
+           minimizaWindow();
+        })
+        
+        //LISTEN BUTTON CLOSE
+        $('.close',settings.win).click(close)
+
+        //LISTEN ICON TASK BAR
+        $('.centerItem','#'+settings.idItemTask).bind('click',function(){
+            if($(settings.win).hasClass('min')){
+               $(settings.win).removeClass('min')
+               animateWindow();
+            }else{
+               minimizaWindow();
+            }
+        })
+
+        //DEFINE THE ZINDEX OF WINDOW
+        $('.window').click(function(e){
+            $('.window').css('zIndex', 10);
+            $(this).css('zIndex', 20)
+        })
+
+		
+    }
+
+
+}
+
+core.Navegacao = {};
+
+core.Navegacao = function(){
+
+	 var classe = this;
+
+	 this.showHide = function(boxHide,boxShow){
+		 var parent = boxHide !='' ?	$(boxHide).parents('.window')	:		$(boxShow).parents('.window');
+		 
+		 $(boxHide,parent).toggle();
+		 $(boxShow,parent).toggle();
+	 }
+
+	 this.voltar = function(){
+		 
+		 $('.hidden').hide();
+		 $('.index').show();
+	 }
+
+};
+
+
+
+var navegacao  = new core.Navegacao();
